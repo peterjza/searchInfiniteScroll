062c8c0533e632001934aaec025b3305fd981559
Peter Jarvis <peterjza@gmail.com>
2024-05-06 08:44:23 +0200

    LINT

--- src/hooks/useFetchImages.ts
+++ src/hooks/useFetchImages.ts
@@ -1,35 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export function useFetchImages(page: number) {
-	const apiKey = '849699968ed280ffa0b80ce8222b1fec';
-	const apiSecret = 'e4860b664f50caac';
+  const [currentImages, setCurrentImages] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<unknown | null>(null);
 
-	const [currentImages, setCurrentImages] = useState<number>(0);
-	const [isLoading, setIsLoading] = useState<boolean>(false);
-	const [error, setError] = useState<unknown | null>(null);
-  
-	const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchQuery}&format=json&nojsoncallback=1&page=${page}`;
+  const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchQuery}&format=json&nojsoncallback=1&page=${page}`;
 
-	useEffect(() => {
-		const fetchImages = async () => {
-			setIsLoading(true);
-			try {
-				const response = await fetch(url);
-				if (!response.ok) {
-					throw new Error('Failed to fetch');
-				}
-				const data = await response.json();
-				setCurrentImages(data);
-			} catch (error: unknown) {
-          if (error instanceof Error) {
-				    setError(error.message);
-					}
-			} finally {
-				setIsLoading(false);
-			}
-		};
-		fetchImages();
-	}, [page]);
+  useEffect(() => {
+    const fetchImages = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Failed to fetch");
+        }
+        const data = await response.json();
+        setCurrentImages(data);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchImages();
+  }, [page]);
 
-	return { currentImages, isLoading, error };
+  return { currentImages, isLoading, error };
 }
